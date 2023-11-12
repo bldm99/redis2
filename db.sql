74e0b9c802d42aa6df3bbea93c190b5b55f9cfeb
bldm99 <bladimir.chipana@tecsup.edu.pe>
2023-11-12 02:34:40 -0500

    post9

---
+++ db.sql
@@ -0,0 +1,16 @@
+-- init.sql
+\connect mydatabase
+
+-- Se crea la tabla 'Cosenos'
+CREATE TABLE Cosenos
+(
+    Id serial PRIMARY KEY,
+    Nombre VARCHAR (255) NOT NULL,
+    Valor_Coseno DOUBLE PRECISION NOT NULL
+);
+
+-- Se cambia el propietario de la tabla a 'myuser'
+ALTER TABLE "Cosenos" OWNER TO myuser;
+
+-- Se insertan datos en la tabla 'Cosenos'
+Insert into Cosenos(Nombre, Valor_Coseno) values( 'Prueba', 0.5);
