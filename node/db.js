3b930ea296e8cb84164ac73f9beb5ca49ee450d3
bldm99 <bladimir.chipana@tecsup.edu.pe>
2023-11-12 13:49:43 -0500

    post16

---
+++ node/db.js
@@ -0,0 +1,17 @@
+
+const { Pool } = require('pg')
+const pool = new Pool({
+    user: 'postgres',
+    host: 'db',
+    database: 'postgres',
+    password: 'postgres',
+    port: 5432,
+  });
+
+  
+
+module.exports = pool;
+
+
+
+  
--- node/index.js
+++ node/index.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const { Pool } = require('pg');
 
 const app = express();
 const port = 3000;
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'db',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5432,
-});
+const pool = require('./db')
+
+
+
 
 app.get('/', (req, res) => {
   // Ejemplo de consulta
