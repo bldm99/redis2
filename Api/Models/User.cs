c5002a1a0b40124b0b08933ea80ca5b63b1b30bb
bldm99 <bladimir.chipana@tecsup.edu.pe>
2023-11-14 19:25:05 -0500

    ratings5

--- Api/Models/User.cs
+++ Api/Models/User.cs
@@ -7,9 +7,15 @@ namespace Models //nesesari
     [Table("users")]
     public class User
     {
-        [Column("id")]
+        /*[Column("id")]
         public int Id { get; set; }
         [Column("nombre")]
+        public string Nombre { get; set; }
+         [Column("valor_coseno")]
+        public double Valor_Coseno { get; set; }*/
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("conteo")]
         public string Nombre { get; set; }
          [Column("valor_coseno")]
         public double Valor_Coseno { get; set; }
